8b5e54ebcfc69ddbc3fc45b977bb65e5bf784b1f
QIANG-WANG-JK <321516900@qq.com>
2019-12-19 13:50:09 +0800

    增添标题栏与侧滑栏联动

--- app/src/main/java/com/hv/heartvoice/View/activity/MainActivity.java
+++ app/src/main/java/com/hv/heartvoice/View/activity/MainActivity.java
@@ -2,10 +2,21 @@ package com.hv.heartvoice.View.activity;
 
 import android.os.Bundle;
 
-import com.hv.heartvoice.Base.BaseCommonActivity;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.hv.heartvoice.Base.BaseTitleActivity;
 import com.hv.heartvoice.R;
 
-public class MainActivity extends BaseCommonActivity {
+import butterknife.BindView;
+
+public class MainActivity extends BaseTitleActivity {
+
+    /**
+     * 侧滑控件
+     */
+    @BindView(R.id.drawerLayout)
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +27,13 @@ public class MainActivity extends BaseCommonActivity {
     @Override
     protected void initViews() {
         super.initViews();
-        hideStatusBar();
+        //侧滑配置
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(getMainActivity(), drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        //添加侧滑监听器
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        //同步状态
+        actionBarDrawerToggle.syncState();
+        //toolbar.setNavigationIcon(R.mipmap.email);设置小按钮图标
     }
+
 }
