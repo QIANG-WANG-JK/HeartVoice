f40c190053116cc87bb948da9f3f60874d1a5109
QIANG-WANG-JK <321516900@qq.com>
2020-01-15 12:57:29 +0800

    添加歌单详情音乐列表

---
+++ app/src/main/java/com/hv/heartvoice/Adapter/SongAdapter.java
@@ -0,0 +1,44 @@
+package com.hv.heartvoice.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.hv.heartvoice.Domain.Song;
+import com.hv.heartvoice.R;
+
+import java.util.List;
+
+/**
+ * 歌单详情适配器
+ */
+public class SongAdapter extends BaseQuickAdapter<Song, BaseViewHolder> {
+
+    /**
+     * 构造方法
+     * @param layoutResId
+     */
+    public SongAdapter(int layoutResId) {
+        super(layoutResId);
+    }
+
+    /**
+     * 显示数据
+     * @param helper
+     * @param item
+     */
+    @Override
+    protected void convert(@NonNull BaseViewHolder helper, Song item) {
+
+        //显示位置
+        helper.setText(R.id.songPosition,String.valueOf(helper.getAdapterPosition() + 1));
+
+        //显示标题
+        helper.setText(R.id.songTitle,item.getTitle());
+
+        //显示歌手
+        helper.setText(R.id.songerTitle,item.getSinger().getNickname());
+
+    }
+}
--- app/src/main/java/com/hv/heartvoice/View/activity/SheetDetailActivity.java
+++ app/src/main/java/com/hv/heartvoice/View/activity/SheetDetailActivity.java
@@ -1,15 +1,21 @@
 package com.hv.heartvoice.View.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hv.heartvoice.Adapter.SongAdapter;
 import com.hv.heartvoice.Base.BaseTitleActivity;
 import com.hv.heartvoice.Domain.Sheet;
 import com.hv.heartvoice.Model.Api;
 import com.hv.heartvoice.Model.myObserver.HttpObserver;
 import com.hv.heartvoice.Model.response.DetailResponse;
 import com.hv.heartvoice.R;
-import com.hv.heartvoice.Util.LogUtil;
-import com.hv.heartvoice.Util.ToastUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 歌单详情界面
@@ -17,6 +23,13 @@ import com.hv.heartvoice.Util.ToastUtil;
 public class SheetDetailActivity extends BaseTitleActivity {
 
     private static final String TAG = "SheetDetailActivity";
+
+    @BindView(R.id.sheetDetailRecyclerView)
+    RecyclerView sheetDetailRecyclerView;
+
+    @BindView(R.id.back)
+    ImageView back;
+
     /**
      * 歌单ID
      */
@@ -26,6 +39,16 @@ public class SheetDetailActivity extends BaseTitleActivity {
      * 歌单数据
      */
     private Sheet data;
+
+    /**
+     * 布局管理器
+     */
+    private LinearLayoutManager layoutManager;
+
+    /**
+     * 适配器
+     */
+    private SongAdapter songAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +63,14 @@ public class SheetDetailActivity extends BaseTitleActivity {
         //禁用ToolBar按钮
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
+        //尺寸固定
+        sheetDetailRecyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(getMainActivity());
+
+        //设置布局管理器
+        sheetDetailRecyclerView.setLayoutManager(layoutManager);
+
     }
 
     @Override
@@ -48,6 +79,12 @@ public class SheetDetailActivity extends BaseTitleActivity {
 
         //获取传递的id
         id = extraId();
+
+        //创建适配器
+        songAdapter = new SongAdapter(R.layout.item_song_detail);
+
+        //设置适配器
+        sheetDetailRecyclerView.setAdapter(songAdapter);
 
         fetchData();
     }
@@ -64,10 +101,20 @@ public class SheetDetailActivity extends BaseTitleActivity {
                         next(data.getData());
                     }
                 });
-
     }
 
     private void next(Sheet data) {
         this.data = data;
+
+        if(data.getSongs() != null && data.getSongs().size() > 0){
+            //设置数据
+            songAdapter.replaceData(data.getSongs());
+        }
     }
+
+    @OnClick(R.id.back)
+    public void back(){
+        onBackPressed();
+    }
+
 }
